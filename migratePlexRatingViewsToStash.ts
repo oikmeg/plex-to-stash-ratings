c54547dde241ebf6947dc5ea292c7d65bfa6011a
oikmeg <oikmeg@protonmail.ch>
2023-05-13 16:30:32 -0500

    feat: :sparkles: add progress bars; use input csv as the first part of matching; use proper csv parsing

--- migratePlexRatingViewsToStash.ts
+++ migratePlexRatingViewsToStash.ts
@@ -1,17 +1,99 @@
 import { promises as fs } from 'fs'
 import { join } from 'path'
-import { parse } from 'json2csv'
 import { GraphQLClient, gql } from 'graphql-request'
+import { parse, stringify } from 'csv'
+import chalk from 'chalk'
+import cliProgress from 'cli-progress'
+
+// Types
+// Type for the json file
+type JsonFile = {
+    data: {
+        allScenes: {
+            id: string
+            path: string
+            title: string
+        }[]
+    }
+}
+// Type for the csv file
+type CsvFile = string[][]
+// Type for the results
+type Results = {
+    id: string
+    path: string
+    title: string
+    views: string
+    rating: string
+    result: string
+    error: string
+}[]
+// Type for the items to update
+type ItemsToUpdate = {
+    id: string
+    path: string
+    title: string
+    views: string
+    rating: string
+}[]
+// Type for the config file
+type Config = {
+    graphql_url: string
+    graphql_api_key: string
+    plex_csv: string
+    stash_json: string
+}
 
 const main = async () => {
-    // Get the config file
-    const config = JSON.parse(
-        await fs.readFile(join(__dirname, 'config.json'), 'utf-8')
+    const configFilePath = join(__dirname, 'config.json')
+    // Make sure the config file exists
+    try {
+        await fs.access(configFilePath)
+    } catch (error) {
+        console.log(
+            'Missing config.json. Please make sure you have config.json, plex.csv, and stash.json in the same directory as this script.'
+        )
+        process.exit(1)
+    }
+
+    const config: Config = JSON.parse(
+        await fs.readFile(configFilePath, 'utf-8')
     )
     const jsonFilePath = join(__dirname, config.stash_json)
     const csvFilePath = join(__dirname, config.plex_csv)
+    // Check that the required files exist and exit if they don't
+    try {
+        await fs.access(jsonFilePath)
+        await fs.access(csvFilePath)
+    } catch (error) {
+        console.log(
+            'Missing one or more required files. Please make sure you have config.json, plex.csv, and stash.json in the same directory as this script.'
+        )
+        process.exit(1)
+    }
+
+    // Create a graphql query for when a rating needs to be updated but not views
+    const queryRating = gql`
+        mutation SceneUpdate($id: ID!, $rating100: Int) {
+            sceneUpdate(input: { id: $id, rating100: $rating100 }) {
+                play_count
+                rating100
+            }
+        }
+    `
+
+    // Create a graphql query for when views need to be updated but not rating
+    const queryViews = gql`
+        mutation SceneUpdate($id: ID!, $play_count: Int) {
+            sceneUpdate(input: { id: $id, play_count: $play_count }) {
+                play_count
+                rating100
+            }
+        }
+    `
+
     // GraphQL query to update the stash scene with the new ratings and views
-    const query = gql`
+    const queryAll = gql`
         mutation SceneUpdate($id: ID!, $rating100: Int, $play_count: Int) {
             sceneUpdate(
                 input: {
@@ -27,82 +109,155 @@ const main = async () => {
     `
     const jsonFile = await fs.readFile(jsonFilePath, 'utf-8')
     const csvFile = await fs.readFile(csvFilePath, 'utf-8')
-    const json = JSON.parse(jsonFile)
-    const csv = csvFile.split('\n').map((line) => line.split('|'))
-    // Remove double quotes from all csv values
-    csv.forEach((line) =>
-        line.forEach((value, index) => (line[index] = value.slice(1, -1)))
-    )
+    const json: JsonFile = JSON.parse(jsonFile)
+    // use csv-parse to parse the csv file into an array, using delimiter | and escape character "
+    const csv: CsvFile = await new Promise((resolve, reject) => {
+        parse(csvFile, { delimiter: '|', escape: '"' }, (error, output) => {
+            if (error) reject(error)
+            resolve(output)
+        })
+    })
+
     // init the graphql client
     const client = new GraphQLClient(config.graphql_url, {
         headers: {
             ApiKey: `${config.graphql_api_key}`,
         },
     })
-    const results = [] as any[]
+    const results: Results = []
+    const itemsToUpdate: ItemsToUpdate = []
 
-    // For each scene in the json file
-    for (const scene of json.data.allScenes) {
-        // Monitor time to update each scene
-        const startTime = performance.now()
-        // Match the scene path to the csv scene path
-        const match = csv.find((line) => line[0] === scene.path)
+    // Create a progress bar with the total number of scenes to update
+    const bar = new cliProgress.SingleBar(
+        {
+            format:
+                'Matching items |' +
+                chalk.green('{bar}') +
+                '| {percentage}% || {value}/{total} Scenes || ETA: {eta_formatted}',
+        },
+        cliProgress.Presets.shades_classic
+    )
+    // Start the progress bar
+    bar.start(csv.length, 0)
+
+    csv.forEach(function (line, index) {
+        // Update the progress bar
+        bar.update(index + 1)
+        const match = json.data.allScenes.find(
+            (scene) => scene.path === line[0]
+        )
 
         if (match) {
-            const [path, title, views, rating] = match
-            // If rating or views is 0, skip it
-            if (rating === '0' || views === '0') {
-                continue
-            }
-
-            // Convert rating to 0-100 in multiples of 10, or 0 if null
-            const rating100 = rating ? parseInt(rating, 10) * 10 : 0
-            // Convert views to int, or 0 if null
-            const play_count = views ? parseInt(views, 10) : 0
-
-            const variables = {
-                id: scene.id,
-                rating100,
-                play_count,
-            }
-
-            try {
-                const result = await client.request(query, variables)
-
-                results.push({
-                    id: scene.id,
-                    path,
-                    title,
-                    views,
-                    rating,
-                    result,
-                })
-            } catch (error) {
-                results.push({
-                    id: scene.id,
-                    path,
-                    title,
-                    views,
-                    rating,
-                    error,
-                })
-            }
-            // Log the time it took to update the scene
-            const endTime = performance.now()
-            console.log(
-                `Updated scene ${scene.id} in ${
-                    Math.floor(endTime) - Math.floor(startTime)
-                }ms`
+            const [path, title, views, rating] = line
+            // If views or rating are not between 1-10 then skip
+            if (
+                (views && (parseInt(views) < 1 || parseInt(views) > 10)) ||
+                (rating && (parseInt(rating) < 1 || parseInt(rating) > 10))
             )
+                return
+
+            // Add the scene to the items to update array
+            itemsToUpdate.push({
+                id: match.id,
+                path,
+                title,
+                views,
+                rating,
+            })
+        }
+    })
+    // Stop the progress bar
+    bar.stop()
+
+    // Console log the number of items to update
+    // colorize the number of items to update red
+    console.log(
+        `Found ${chalk.red(
+            itemsToUpdate.length
+        )} items to update out of ${chalk.yellow(
+            json.data.allScenes.length
+        )} total scenes in Stash`
+    )
+
+    // Make a progress bar for updating the scenes
+    const updateBar = new cliProgress.SingleBar(
+        {
+            format:
+                'Updating scenes |' +
+                chalk.green('{bar}') +
+                '| {percentage}% || {value}/{total} Scenes || ETA: {eta_formatted}',
+        },
+        cliProgress.Presets.shades_classic
+    )
+
+    // Start the progress bar
+    updateBar.start(itemsToUpdate.length, 0)
+
+    // For each item to update
+    for (const item of itemsToUpdate) {
+        // Update the progress bar
+        updateBar.update(results.length + 1)
+        // Monitor time to update each scene
+        const startTime = performance.now()
+        // Find the right query to use
+        const query =
+            item.views && item.rating
+                ? queryAll
+                : item.views
+                ? queryViews
+                : queryRating
+        // Create the variables for the query
+        const variables = {
+            id: item.id,
+            rating100: item.rating ? parseInt(item.rating) * 10 : null,
+            play_count: item.views ? parseInt(item.views) : null,
+        }
+
+        try {
+            // Run the query
+            const data = await client.request(query, variables)
+            // Push the results to the results array
+            results.push({
+                id: item.id,
+                path: item.path,
+                title: item.title,
+                views: item.views,
+                rating: item.rating,
+                result: JSON.stringify(data),
+                error: '',
+            })
+        } catch (error) {
+            // Push the error to the results array
+            results.push({
+                id: item.id,
+                path: item.path,
+                title: item.title,
+                views: item.views,
+                rating: item.rating,
+                result: '',
+                error: JSON.stringify(error),
+            })
         }
     }
+    // Stop the progress bar
+    updateBar.stop()
 
-    // Write the results to a csv file
-    const csvResults = parse(results, {
-        fields: ['id', 'path', 'title', 'views', 'rating', 'result', 'error'],
-    })
+    // use csv-generate to convert the results array into a csv file
+    const csvResults = (await new Promise((resolve, reject) => {
+        stringify(results, { header: true }, (error, output) => {
+            if (error) reject(error)
+            resolve(output)
+        })
+    })) as string
 
     await fs.writeFile('results.csv', csvResults)
+
+    // Tell the user the results are in the results.csv file
+    console.log(
+        `Results are in the ${chalk.green(
+            'results.csv'
+        )} file in the current directory`
+    )
 }
 
 main()
